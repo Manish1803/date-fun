e735d2671227479d425cdd86f535fe14eddef98b
Manish Patil <patilmanishp18@gmail.com>
2024-10-14 10:31:37 +0530

    initial commit ✨

---
+++ index.js
@@ -0,0 +1,32 @@
+export function getFormattedTimeStamp() {
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  const current = new Date();
+  const date = current.getDate();
+  const month = months[current.getMonth()];
+  const year = current.getFullYear();
+
+  let hours = current.getHours();
+  let minutes = current.getMinutes().toString().padStart(2, "0");
+
+  const ampm = hours >= 12 ? "PM" : "AM";
+  hours = hours % 12 || 12;
+
+  const datePart = `${date} ${month} ${year}`;
+  const timePart = `${hours}:${minutes} ${ampm}`;
+
+  return { datePart, timePart };
+}
